51599bdc97c3a12b412c238acce45a7715d10035
Ashley Hendrata <134671782+ashhendrata@users.noreply.github.com>
2023-11-12 14:44:24 -0500

    Time: 10 ms (83.59%), Space: 54.7 MB (77.64%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> hashset = new HashSet<>();
+        for (int num : nums){
+            if (hashset.contains(num)){
+                return true;
+            } else {
+                hashset.add(num);
+            }
+        }
+        return false;
+    } 
+}
