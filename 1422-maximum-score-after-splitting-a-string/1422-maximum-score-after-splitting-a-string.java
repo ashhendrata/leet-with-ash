61640e65f811ef7186bb94deb8d436305cc69c43
Ashley Hendrata <ashley.m.hendrata@gmail.com>
2023-12-22 20:57:08 -0500

    Time: 1 ms (97.83%), Space: 41.5 MB (33.04%) - LeetHub

--- 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
+++ 1422-maximum-score-after-splitting-a-string/1422-maximum-score-after-splitting-a-string.java
@@ -1,32 +1,22 @@
 class Solution {
     public int maxScore(String s) {
-        HashMap<String, String> stringMap = new HashMap<>();
-        for (int i = 1; i < s.length(); i++){
-            String left = s.substring(0, i);
-            String right = s.substring(i);
-            stringMap.put(left, right);
-            //System.out.println(left + " " + right);
+        int numOnes = 0;
+        for (int i = 0; i < s.length(); i++){
+            if (s.charAt(i) == '1'){
+                numOnes++;
+            }
 
         }
+        int numZeros = 0;
         int max = 0;
-        int score = 0;
-        for (String key: stringMap.keySet()){
-            for (int i = 0; i < key.length(); i++){
-                if (String.valueOf(key.charAt(i)).equals("0")){
-                    score++;
-                }
+        for (int i = 0; i < s.length()-1; i++){
+            if (s.charAt(i) == '1'){
+                numOnes--;
+            } else {
+                numZeros++;
             }
-            for (int i = 0; i < (stringMap.get(key)).length(); i++){
-                if (String.valueOf((stringMap.get(key)).charAt(i)).equals("1")){
-                    score++;
-                }
-            }
-            if (score > max){
-                max = score;
-            }
-            score = 0;
+            max = Math.max(max, numOnes+numZeros);
         }
         return max;
-        
     }
 }
