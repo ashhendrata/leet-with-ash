7cafa02baa2015f921fe71fb0fd2446842444db2
Ashley Hendrata <ashley.m.hendrata@gmail.com>
2024-03-20 20:32:48 -0400

    Time: 15 ms (26.71%), Space: 44.4 MB (44.22%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,18 +1,27 @@
 class Solution {
-public boolean isAnagram(String s, String t) {
-    if (s.length() != t.length()) {
-        return false;
-    }
-    int[] counter = new int[26];
-    for (int i = 0; i < s.length(); i++) {
-        counter[s.charAt(i) - 'a']++;
-        counter[t.charAt(i) - 'a']--;
-    }
-    for (int count : counter) {
-        if (count != 0) {
-            return false;
+    public boolean isAnagram(String s, String t) {
+        HashMap<Character, Integer> freq_map = new HashMap<>();
+        for (int i = 0; i < s.length(); i++){
+            Character curr = s.charAt(i);
+            if (!freq_map.containsKey(curr)){
+                freq_map.put(curr, 1);
+            } else {
+                freq_map.put(curr, freq_map.get(curr)+1);
+            }
         }
+        for (int i = 0; i < t.length(); i++){
+            Character t_curr = t.charAt(i);
+            if (!freq_map.containsKey(t_curr)){
+                return false;
+            } else {
+                freq_map.put(t_curr, freq_map.get(t_curr) - 1);
+            }
+        }
+        for (Character key: freq_map.keySet()){
+            if (freq_map.get(key) != 0){
+                return false;
+            }
+        }
+        return true;
     }
-    return true;
-}
 }
